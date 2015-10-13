08afb826b2ed7c44ccf525cef63c877a49713e78
Jim Kang <jimkang@gmail.com>
2015-10-13 09:57:47 -0400

    List update.

--- defaultlists.js
+++ defaultlists.js
@@ -73,7 +73,11 @@ var lists = {
     'slaveowners',
     'condolence',
     'condolences',
-    'RIP'
+    'RIP',
+    'molestation',
+    'molestations',
+    'molester',
+    'molesters'
   ],
   tragedyModeBlacklist: [
     'gaza',
